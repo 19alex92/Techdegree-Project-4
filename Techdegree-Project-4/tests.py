ca9d61d0ef666131fa6252418b141162189ad5c2
19alex92 <46493443+19alex92@users.noreply.github.com>
2019-02-09 11:30:53 +0100

    inital commit tests.py

---
+++ Techdegree-Project-4/tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+import work_log
+import algorithm
+
+test = Search()
+
+class WorkLogTests(unittest.TestCase):
+
+
+
+class AlgorithmTests(unittest.TestCase):
+
+    def test_search_for_employee(self):
+        self.assertEqual(test.search_employee())
